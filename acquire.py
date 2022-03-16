47710ab1f271d8668ddc3539d6635b345bf93d97
ellaxu43 <ella.xu43@gmail.com>
2022-03-16 14:10:11 -0500

    add more files

---
+++ acquire.py
@@ -0,0 +1,72 @@
+import os
+import pandas as pd
+import env
+
+database_url_base = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/'
+
+
+def get_telco_data(use_cache=True):
+    if os.path.exists('telco.csv') and use_cache:
+        print('Using cached csv')
+        return pd.read_csv('telco.csv')
+    print('Acquiring data from SQL database')
+    query = '''
+    SELECT *
+    FROM customers
+    JOIN internet_service_types USING (internet_service_type_id)
+    JOIN contract_types USING (contract_type_id)
+    JOIN payment_types USING (payment_type_id)
+    '''
+    df = pd.read_sql(query, database_url_base + 'telco_churn')
+    df.to_csv('telco.csv', index=False)
+    return df
+
+def split_telco_data(df):
+    '''
+    This function performs split on telco data, stratify churn.
+    Returns train, validate, and test dfs.
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.churn)
+    train, validate = train_test_split(train_validate, test_size=.2, 
+                                   random_state=123, 
+                                   stratify=train_validate.churn)
+    return train, validate, test
+
+def prep_telco_data(df):
+    # Drop duplicate columns
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], inplace=True)
+       
+    # Drop null values stored as whitespace    
+    df['total_charges'] = df['total_charges'].str.strip()
+    df = df[df.total_charges != '']
+    
+    # Convert to correct datatype
+    df['total_charges'] = df.total_charges.astype(float)
+    
+    # Convert binary categorical variables to numeric
+    df['gender_encoded'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['partner_encoded'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['dependents_encoded'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['phone_service_encoded'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['paperless_billing_encoded'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['churn_encoded'] = df.churn.map({'Yes': 1, 'No': 0})
+    
+    # Get dummies for non-binary categorical variables
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type']], dummy_na=False, \
+                              drop_first=True)
+    
+    # Concatenate dummy dataframe to original 
+    df = pd.concat([df, dummy_df], axis=1)
+    
+    return df
